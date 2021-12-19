8f3af0d987753d08e73b6980dbaf8de80a28e18c
Shaykh Jake <ShaykhJake@gmail.com>
2021-12-19 11:56:54 -0500

    Doc comments for Port class

--- NetworkMapper/src/networkMapper/Port.java
+++ NetworkMapper/src/networkMapper/Port.java
@@ -1,10 +1,25 @@
 package networkMapper;
 
+/**
+ * Object to represent each port on a host
+ * @author Jake @ PidginForge
+ *
+ */
 public class Port {
+	/**
+	 * Port number
+	 */
 	int port;
+	/**
+	 * Whether or not the port is open/active
+	 */
 	boolean isOpen;
-	String service;
 	
+	/**
+	 * Constructor
+	 * @param port
+	 * @param isOpen
+	 */
 	public Port(int port, boolean isOpen) {
 		this.port = port;
 		this.isOpen = isOpen;
